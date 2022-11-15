a3c03060264d0de0a380eb69b450019d8dce20a4
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-11-15 10:06:13 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [entries, setEntries] = useState([]);
+
+  const addEntryToPhoneBook = (entry) => {
+    setEntries([...entries, entry].sort((a, b) => a.lastName.toLowerCase() > b.lastName.toLowerCase() ? 1 : -1
+    )
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Phone Book App</h1>
+      <EntryForm addEntryToPhoneBook={addEntryToPhoneBook} />
+      <DisplayEntries entries={entries} />
     </div>
   );
+}
+
+function EntryForm({addEntryToPhoneBook}) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addEntryToPhoneBook({ firstName, lastName, phoneNumber })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className='input__div'>
+        <input className='input'
+          type='text' name='firstName' id='firstName' placeholder='First Name' value={firstName}
+          onChange={(e) => setFirstName(e.target.value)} />
+        <input className='input'
+          type='text' name='lastName' id='lastName' placeholder='Last Name' value={lastName}
+          onChange={(e) => setLastName(e.target.value)} />
+        <input className='input'
+          type='text' name='phoneNumber' id='phoneNumber' placeholder='Phone Number' value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)} />
+        <button className='submit__button' type='submit'>Submit</button>
+      </div>
+    </form>
+  )
+}
+
+function DisplayEntries({ entries }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Phone Number</th>
+        </tr>
+      </thead>
+      <tbody>
+        {entries.map((entry) => (
+          <tr>
+            <td>{entry.firstName}</td>
+            <td>{entry.lastName}</td>
+            <td>{entry.phoneNumber}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
 
 export default App;
